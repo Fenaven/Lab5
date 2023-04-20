4a876842a33d6d2edd01c0a9e9b50c1bc1a56335
Fenaven <115934931+Fenaven@users.noreply.github.com>
2023-04-20 21:24:54 +0300

    Add files via upload

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"madeby.client"},{"l":"madeby.client.util"},{"l":"madeby.common.commands"},{"l":"madeby.common.data.data_class"},{"l":"madeby.common.Exception"},{"l":"madeby.common.util"},{"l":"madeby.server"}];updateSearchResults();
